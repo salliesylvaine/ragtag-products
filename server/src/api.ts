2576d83ae37c48b7faf19f476a3b6731ecb5dfb0
Sallie McKee <salliemckee7@gmail.com>
2022-08-16 13:55:09 -0500

    decoded jwt with express and authenticated api

--- server/src/api.ts
+++ server/src/api.ts
@@ -49,6 +49,7 @@ function runAsync(callback: Function) {
 
 import { createPaymentIntent } from "./payments";
 import { handleStripeWebhook } from "./webhooks";
+import { auth } from "firebase-admin";
 
 //Payment Intents API
 
@@ -64,3 +65,36 @@ app.post(
 
 //Handle Webhooks
 app.post("/hooks", runAsync(handleStripeWebhook));
+
+//Decodes the Firebase JSON Web Token
+app.use(decodeJWT);
+// Decodes the JSON web token sent via the frontend app
+//Makes the currentUser (firebase) data available on the body
+//This is used as middleware to intercept the incomnig request
+//and modify it
+async function decodeJWT(req: Request, res: Response, next: NextFunction) {
+  if (req.headers?.authorization?.startsWith("Bearer")) {
+    const idToken = req.headers.authorization.split("Bearer")[1];
+
+    try {
+      const decodedToken = await auth().verifyIdToken(idToken);
+      req["currentUser"] = decodedToken;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  next();
+}
+
+//Validate User
+
+//Throws and error if the currentUser does not exist on the request
+function validateUser(req: Request) {
+  const user = req["currentUser"];
+  if (!user) {
+    throw new Error(
+      "You must be logged in to make this request. i.e. Authorization: Bearer <token>"
+    );
+  }
+  return user;
+}
--- stripe-react/src/helpers.js
+++ stripe-react/src/helpers.js
@@ -1,15 +1,24 @@
+import { auth } from "./firebase";
 const API = "http://localhost:3333";
 
 //A helper function to fetch data from your API.
+//It sets the Firebase auth token on the request.
 export async function fetchFromAPI(endpointURL, opts) {
   const { method, body } = { method: "POST", body: null, ...opts };
+
+  const user = auth.currentUser;
+  const token = user && (await user.getIdToken());
 
   const res = await fetch(`${API}/${endpointURL}`, {
     method,
     ...API(body && { body: JSON.stringify(body) }),
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
   });
+
+  //Now every API call that goes to our backend has the context
+  //for the current user that executed that call from the frontend
   return res.json();
 }

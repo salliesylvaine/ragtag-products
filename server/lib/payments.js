f55c81f369931d6a35f741731985c823bf5cf5f5
Sallie McKee <salliemckee7@gmail.com>
2022-08-23 10:46:52 -0500

    added createPaymentIntentAndCharge function in payments.ts

--- server/lib/payments.js
+++ server/lib/payments.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPaymentIntent = void 0;
+exports.createPaymentIntentAndCharge = exports.createPaymentIntent = void 0;
 const _1 = require("./");
 //Create a Payment Intent with a specific amount
 async function createPaymentIntent(amount) {
@@ -8,8 +8,22 @@ async function createPaymentIntent(amount) {
         amount,
         currency: "usd",
     });
-    paymentIntent.status;
+    // paymentIntent.status;
     return paymentIntent;
 }
 exports.createPaymentIntent = createPaymentIntent;
+//Create a Payment Intent and attempt to charge right away,
+//must have an existing customer with a saved card payment method on file.
+async function createPaymentIntentAndCharge(amount, customer, payment_method) {
+    const paymentIntent = await _1.stripe.paymentIntents.create({
+        amount,
+        customer,
+        payment_method,
+        currency: "usd",
+        off_session: true,
+        confirm: true,
+    });
+    return paymentIntent;
+}
+exports.createPaymentIntentAndCharge = createPaymentIntentAndCharge;
 //# sourceMappingURL=payments.js.map
--- server/src/payments.ts
+++ server/src/payments.ts
@@ -7,7 +7,25 @@ export async function createPaymentIntent(amount: number) {
     currency: "usd",
   });
 
-  paymentIntent.status;
+  // paymentIntent.status;
 
+  return paymentIntent;
+}
+
+//Create a Payment Intent and attempt to charge right away,
+//must have an existing customer with a saved card payment method on file.
+export async function createPaymentIntentAndCharge(
+  amount: number,
+  customer: string,
+  payment_method: string
+) {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    customer,
+    payment_method,
+    currency: "usd",
+    off_session: true,
+    confirm: true,
+  });
   return paymentIntent;
 }
